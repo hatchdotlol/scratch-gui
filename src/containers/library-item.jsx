29af5833d4f9cd7e498fc80d72f9f6b7c720fc16
raynecloudy <rayne@raynec.dev>
2025-04-07 14:32:05 -0700

    remove confirmation message

--- src/containers/library-item.jsx
+++ src/containers/library-item.jsx
@@ -48,16 +48,6 @@ class LibraryItem extends React.PureComponent {
             return;
         }
 
-        if (
-            !this.props.favorite &&
-            this.props.incompatibleWithScratch &&
-            !e.shiftKey &&
-            // eslint-disable-next-line no-alert
-            !confirm(this.props.intl.formatMessage(messages.incompatible))
-        ) {
-            return;
-        }
-
         if (!this.props.disabled) {
             if (this.props.href) {
                 window.open(this.props.href);
